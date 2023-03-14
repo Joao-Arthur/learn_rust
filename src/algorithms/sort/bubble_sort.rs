2adf655d997a653ba54f41d53be048ac4d970a4f
joao-arthur <joao.lothamer@gmail.com>
2023-03-14 20:38:56 -0300

    bubble sort

---
+++ src/algorithms/sort/bubble_sort.rs
@@ -0,0 +1,42 @@
+pub fn bubble_sort<T: Ord>(arr: &mut [T]) {
+    if arr.is_empty() {
+        return;
+    }
+    let mut sorted = false;
+    let mut n = arr.len();
+    while !sorted {
+        sorted = true;
+        for i in 0..n - 1 {
+            if arr[i] > arr[i + 1] {
+                arr.swap(i, i + 1);
+                sorted = false;
+            }
+        }
+        n -= 1;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn descending() {
+        let mut values = vec![6, 5, 40, 3, 2, 1];
+        bubble_sort(&mut values);
+        assert_eq!(values, vec![1, 2, 3, 5, 6, 40]);
+    }
+
+    #[test]
+    fn ascending() {
+        let mut values = vec![1, 2, 3, 4, 5, 6];
+        bubble_sort(&mut values);
+        assert_eq!(values, vec![1, 2, 3, 4, 5, 6]);
+    }
+    #[test]
+    fn empty() {
+        let mut values: Vec<usize> = vec![];
+        bubble_sort(&mut values);
+        assert_eq!(values, vec![]);
+    }
+}
